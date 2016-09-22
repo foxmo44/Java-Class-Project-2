f961cc5cb1c466093a002db755508447cfb2007e
foxmo <foxmo44@yahoo.com>
2016-09-21 21:58:30 -0500

    commented the itemtag

--- src/ItemTag.java
+++ src/ItemTag.java
@@ -5,7 +5,9 @@
 // DateDue: 2016.09.14
 /////////////////////////////////////////////////////////////////////////////////////////
 
-//This is a generic Tag that can be used with the list or table
+/**
+ * This is a generic Tag that can be used with the list or table
+ */
 public class ItemTag extends Tag{
 
     //Default constructor
@@ -13,10 +15,16 @@ public class ItemTag extends Tag{
 
     }
 
+    /**
+     * @param c input string for the constructor for the generic tag class
+     */
     public ItemTag( String c){
         super(c);  //calls the base class constructor.  must be first
     }
 
+    /**
+     * @return  return the tag content
+     */
     public String toString()    //Must be provided
     {
         return getContent();
